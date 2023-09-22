1957df42a9c7cb90059199ba705dd1a01996e6b4
RiwEz <ttangun1@gmail.com>
2023-09-22 17:08:21 +0700

    feat: add some treesitter movement

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,10 +1,27 @@
 -- Tree sitter
 
-local treesitter = require 'nvim-treesitter';
+require 'nvim-treesitter';
 
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "rust", "go", "svelte" },
-	auto_install = false,
-	highlight = { enable = true, additional_vim_regex_highlighting = false },
-	indent = { enable = true }
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "rust", "go", "svelte" },
+  auto_install = false,
+  highlight = { enable = true, additional_vim_regex_highlighting = false },
+  indent = { enable = true },
+
+  move = {
+    enable = true,
+    set_jumps = true,
+    goto_next_start = {
+      [']m'] = '@function.outer',
+    },
+    goto_next_end = {
+      [']M'] = '@function.outer',
+    },
+    goto_previous_start = {
+      ['[m'] = '@function.outer',
+    },
+    goto_previous_end = {
+      ['[M'] = '@function.outer',
+    },
+  }
 }
